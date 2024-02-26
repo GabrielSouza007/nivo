aadd7d4197b74f8911603eaa7d42049575dbd47d
Gabriel Souza <gabrielmacielsouza537@gmail.com>
2024-02-26 15:51:47 -0300

    feat: create new tag

--- src/app.tsx
+++ src/app.tsx
@@ -80,7 +80,7 @@ export function App() {
               <Dialog.Content className='fixed space-y-10 p-10 right-0 top-0 bottom-0 h-screen min-w-[320px] z-10 bg-zinc-950 border-l border-zinc-900'>
                 <div className='space-y-3'>
                   <Dialog.Title className='text-xl font-bold'>
-                    Create tag
+                    Create a tag
                   </Dialog.Title>
                   <Dialog.Description className='text-sm text-zinc-500 w-80'>
                     Tags can be used to group videos about similar concepts
